5d92b8ae7bbca0482b8b441868cd05d36ca5f07d
Alexander Naumov <me@alexandernaumov.de>
2019-01-13 17:15:13 +0100

    add mautic form ce

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -15,6 +15,14 @@ array_insert( $GLOBALS['BE_MOD'], 4, [
     ]
 ]);
 
+array_insert( $GLOBALS['TL_CTE'], 4, [
+
+    'mautic' => [
+
+        'mautic_form' => 'Alnv\MauticBundle\Elements\ContentMauticForm'
+    ]
+]);
+
 $GLOBALS['TL_HOOKS']['generatePage'][] = [ 'Alnv\MauticBundle\Hooks\Page', 'onPageGeneration' ];
 $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = [ 'Alnv\MauticBundle\Hooks\Inserttag', 'parse' ];
 $GLOBALS['TL_HOOKS']['processFormData'][] = [ 'Alnv\MauticBundle\Hooks\Form', 'process' ];
