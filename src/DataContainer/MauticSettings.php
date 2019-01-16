e04636c6b573504746e8b99de1b400acb2fd75b0
Alexander Naumov <me@alexandernaumov.de>
2019-01-16 09:45:21 +0100

    add url validator

--- src/DataContainer/MauticSettings.php
+++ src/DataContainer/MauticSettings.php
@@ -15,4 +15,20 @@ class MauticSettings {
             $GLOBALS['TL_DCA']['tl_mautic_settings']['palettes']['default'] = str_replace( '{newsletter_settings},mauticUseNewsletter', '', $GLOBALS['TL_DCA']['tl_mautic_settings']['palettes']['default'] );
         }
     }
+
+
+    public function validateUrl( $strValue ) {
+
+        if ( !$strValue ) {
+
+            return $strValue;
+        }
+
+        if ( substr( $strValue, -1 ) == '/' ) {
+
+            throw new \Exception( $GLOBALS['TL_LANG']['MSC']['mautic_invalid_base_url'] );
+        }
+
+        return $strValue;
+    }
 }
